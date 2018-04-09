0093e2cb3f4147a6f68dad12c16dd6b631a9d084
WolfLee <me@wolfl.ee>
2018-04-09 16:26:34 +0800

    Update zsh/proxy.zsh

--- zsh/proxy.zsh
+++ zsh/proxy.zsh
@@ -4,3 +4,5 @@ if [ "$(lsof -n -i4TCP:$PROXY_PORT | grep LISTEN)" ];then
   export http_proxy="http://localhost:$PROXY_PORT"
   export https_proxy="http://localhost:$PROXY_PORT"
 fi
+
+export no_proxy="localhost,127.0.0.1"
