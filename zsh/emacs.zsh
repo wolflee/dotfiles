c6a0cb14da29da1c61a5bbe9105f7e7ee02cf4e9
WolfLee <me@wolfl.ee>
2019-07-04 10:07:24 -0400

    Update e in emacs.zsh for Linux environment

--- zsh/emacs.zsh
+++ zsh/emacs.zsh
@@ -1,3 +1,13 @@
 if [[ `uname` == 'Darwin' ]];then
   alias e='open -a /Applications/Emacs.app "$@"'
 fi
+
+if [[ `uname` == 'Linux' ]];then
+    function e {
+        if [ "$(ps axu | grep emacs | grep daemon)" ]; then
+            emacsclient --no-wait $@
+        else
+            emacsclient --alternate-editor="" -nc $@
+        fi
+    }
+fi
